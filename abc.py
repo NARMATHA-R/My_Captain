f24ceecd2ae15793c0ba51fc03b297d980cf134c
NARMATHA-R <94796293+NARMATHA-R@users.noreply.github.com>
2021-11-26 09:56:33 +0530

    created abc.py

---
+++ abc.py
@@ -0,0 +1,8 @@
+
+filename = input("Input the Filename: ")
+f_extns = filename.split(".")
+print ("The extension of the file is : " + repr(f_extns[-1]))
+
+OUTPUT
+Input the Filename: abc.python
+The extension of the file is : 'python'
