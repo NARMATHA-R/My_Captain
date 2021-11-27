4ea07134d5ed9240b977a21f8466449dbd13bfec
NARMATHA-R <94796293+NARMATHA-R@users.noreply.github.com>
2021-11-27 10:43:09 +0530

    updated area.py

--- area.py
+++ area.py
@@ -4,6 +4,6 @@ r=float(input("Input the radius of the circle : "))
 a=p*r*r
 print("The area of the circle with radius",r,"is :" ,a)
 
-OUTPUT:
+#OUTPUT:
 Input the radius of the circle : 1.1
 The area of the circle with radius 1.1 is : 3.8013271108436504
