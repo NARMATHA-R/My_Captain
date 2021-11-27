3dbe535ccae6861e7cbb709d68867e36343f7fa1
NARMATHA-R <94796293+NARMATHA-R@users.noreply.github.com>
2021-11-27 10:37:32 +0530

    Updated abc.py

--- abc.py
+++ abc.py
@@ -2,6 +2,6 @@ filename = input("Input the Filename: ")
 extns = filename.split(".") # split()- Splits the string at the specified separator, and returns a list
 print ("The extension of the file is : " ,extns[-1])# -1 indicates the last element in a sequence
 
-OUTPUT
+#OUTPUT
 Input the Filename: abc.python
 The extension of the file is :  python
