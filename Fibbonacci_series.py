43d8e62bf59db93a955ab495c5fbc5991ca01baa
NARMATHA-R <94796293+NARMATHA-R@users.noreply.github.com>
2021-11-27 10:36:23 +0530

    Created Fibonacci_series.py

---
+++ Fibbonacci_series.py
@@ -0,0 +1,27 @@
+#Python program to generate Fibonacci series until 'n' value
+n = int(input("Enter the value of 'n': "))
+a = 0
+b = 1
+sum = 0
+count = 1
+print("Fibonacci Series: ",sum)
+while(count <= n):
+  print(sum)
+  count += 1
+  a = b
+  b = sum
+  sum = a + b
+  
+  #OUTPUT
+ Enter the value of 'n': 10
+Fibonacci Series:  0
+0
+1
+1
+2
+3
+5
+8
+13
+21
+34
