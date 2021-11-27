6b79e7b858bb0cf966027b831ec410b43f5e3203
NARMATHA-R <94796293+NARMATHA-R@users.noreply.github.com>
2021-11-27 12:18:25 +0530

    created positive_numbers.py

---
+++ positive_numbers.py
@@ -0,0 +1,13 @@
+list1 = [12,-7,5,64,-14]
+for number in list1:
+    if number >=0:
+        print(number,end =' ')# the end=' ' is used to place a space after value instead of a newline.
+        
+#output:12 5 64 
+
+list2 = [12,14,-95,3]
+for number in list2:
+    if number >=0:
+        print(number,end =' ')
+        
+#output:12 14 3 
