e706f7e482613e2b857271dfe1ff84a10c1e4745
NARMATHA-R <94796293+NARMATHA-R@users.noreply.github.com>
2021-11-27 12:40:23 +0530

    created positive_numbers.py

--- positive_numbers.py
+++ positive_numbers.py
@@ -1,3 +1,4 @@
+#1st program
 list1 = [12,-7,5,64,-14]
 for number in list1:
     if number >=0:
@@ -5,9 +6,9 @@ for number in list1:
         
 #output:12 5 64 
 
+#2nd program
 list2 = [12,14,-95,3]
-for number in list2:
-    if number >=0:
-        print(number,end =' ')
+positive_list = [number for number in list2 if number >=0]
+print(positive_list)
         
-#output:12 14 3 
+#output:[12, 14, 3] 
